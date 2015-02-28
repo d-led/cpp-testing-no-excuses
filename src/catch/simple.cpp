5c9e34b974e9e42f99eefcc3d57628d0f5fadb88
Dmitry Ledentsov <dmlled@yahoo.com>
2015-02-28 14:22:16 +0100

    more tests

--- src/catch/simple.cpp
+++ src/catch/simple.cpp
@@ -9,11 +9,13 @@ TEST_CASE("counting up", "[basics]") {
     auto c = counter<>();
     REQUIRE(c.next() == 0);
     CHECK(c.next() == 1);
+
+    static_assert(std::is_same<decltype(c.next()), int>::value, "default counter is int-based");
 }
 
 TEST_CASE("boundary cases", "[boundary]") {
 
-    SECTION("wrapping","[undefined]") {
+    SECTION("wrapping", "[undefined]") {
         auto c = counter<int>(std::numeric_limits<int>::max());
         CHECK(c.next() == std::numeric_limits<int>::max());
         // can't trust that one
@@ -21,9 +23,17 @@ TEST_CASE("boundary cases", "[boundary]") {
     }
 
     SECTION("negative starting values") {
-        auto c = counter<int>(-1);
-        CHECK(c.next() == -1);
-        CHECK(c.next() == 0);
+        SECTION("passing zero") {
+            auto c = counter<int>(-1);
+            CHECK(c.next() == -1);
+            CHECK(c.next() == 0);
+        }
+
+        SECTION("moving up") {
+            auto c = counter<int>(-2);
+            CHECK(c.next() == -2);
+            CHECK(c.next() == -1);
+        }
     }
 
     SECTION("unsigned counter") {
