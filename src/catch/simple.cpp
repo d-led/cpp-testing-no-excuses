0a40523a4a9f2c34211becf58c009f4d0137d750
Dmitry Ledentsov <dmlled@yahoo.com>
2015-02-28 14:11:00 +0100

    more tests

--- src/catch/simple.cpp
+++ src/catch/simple.cpp
@@ -3,8 +3,32 @@
 
 #include <counter/counter.h>
 
-TEST_CASE("counting up","[basics]") {
+#include <limits>
+
+TEST_CASE("counting up", "[basics]") {
     auto c = counter<>();
     REQUIRE(c.next() == 0);
     CHECK(c.next() == 1);
+}
+
+TEST_CASE("boundary cases", "[boundary]") {
+
+    SECTION("wrapping","[undefined]") {
+        auto c = counter<int>(std::numeric_limits<int>::max());
+        CHECK(c.next() == std::numeric_limits<int>::max());
+        // can't trust that one
+        CHECK(c.next() == std::numeric_limits<int>::min());
+    }
+
+    SECTION("negative starting values") {
+        auto c = counter<int>(-1);
+        CHECK(c.next() == -1);
+        CHECK(c.next() == 0);
+    }
+
+    SECTION("unsigned counter") {
+        auto c = counter<unsigned>(std::numeric_limits<unsigned>::max());
+        CHECK(c.next() == std::numeric_limits<unsigned>::max());
+        CHECK(c.next() == 0u);
+    }
 }
--- src/counter/counter.h
+++ src/counter/counter.h
@@ -2,11 +2,14 @@
 
 template<typename number_t = int>
 class counter {
-    number_t count = 0;
+    number_t next_number = 0;
+public:
+    counter() = default;
+    counter(number_t starting_value) : next_number(starting_value) {}
 public:
     number_t next() {
-        auto res = count;
-        count++;
+        auto res = next_number;
+        next_number++;
         return res;
     }
 };
