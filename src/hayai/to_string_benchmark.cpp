d9a441b39f1efe955c5bdd8e04b908a6c43715dd
Dmitry Ledentsov <dmlled@yahoo.com>
2015-03-02 15:19:39 +0100

    Update to_string_benchmark.cpp

--- src/hayai/to_string_benchmark.cpp
+++ src/hayai/to_string_benchmark.cpp
@@ -7,10 +7,10 @@ BENCHMARK(Stringify, stringstream, 1000, 3000)
 {
     std::stringstream ss;
     ss<<42;
-    static std::string res = ss.str();
+    auto res = ss.str();
 }
 
 BENCHMARK(Stringify, lexical_cast, 1000, 3000)
 {
-    static std::string res = boost::lexical_cast<std::string>(42);
+    auto res = boost::lexical_cast<std::string>(42);
 }
